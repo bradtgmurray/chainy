84f2ca3cf128901710ea10ab18b808fa375419c9
System Administrator <bradtgmurray@gmail.com>
2009-01-08 17:37:22 -0500

    Initial prototype of site layout

--- chainy/chainyapp/models.py
+++ chainy/chainyapp/models.py
@@ -8,6 +8,7 @@ class Chain(models.Model):
 
 class Post(models.Model):
     chain = models.ForeignKey(Chain)
+    poster = models.CharField(max_length=50)
     time = models.DateTimeField()
     body = models.CharField(max_length=1000)
 
--- chainy/chainyapp/views.py
+++ chainy/chainyapp/views.py
@@ -1,7 +1,15 @@
+from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse
+from chainy.chainyapp.models import Chain
 
 def main(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    chain_list = Chain.objects.all()
+
+    return render_to_response('chainyapp/templates/main.html', 
+        {'chain_list' : chain_list})
 
 def chain(request, chain_id):
-    return HttpResponse("Viewing chain detail " + chain_id)
+    chain = get_object_or_404(Chain, pk=chain_id)
+    
+    return render_to_response('chainyapp/templates/chain.html', 
+        {'chain' : chain})
