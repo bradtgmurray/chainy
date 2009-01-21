df5d3336abaafd77012a6e33997be749d9414225
brad <bradtgmurray@gmail.com>
2009-01-20 20:36:01 -0500

    Basic mail processing works. Well, works in a way. Subject is not parsed correctly, none of the values are correct, but at least there's the right number of incorrect values

--- chainy_mail/chainymail.py
+++ chainy_mail/chainymail.py
@@ -1,13 +1,26 @@
 import asyncore
 from smtpd import SMTPServer
 
+chainy_mail_domain = 'chainy.neckbeard.ca'
+master_inbox = 'master@' + chainy_mail_domain
+
 class MailServer(SMTPServer):
     def __init__(self, output_connection):
-        SMTPServer.__init__(self, ('localhost', 8000), None)
+        SMTPServer.__init__(self, ('localhost', 8001), None)
         self.output_connection = output_connection
 
     def process_message(self, peer, mailfrom, rcpttos, data):
-        print peer + mailfrom + rcpttos + data
+        processed = False
+        for address in rcpttos:
+            inbox, domain = address.split('@')[:2]
+            if domain == chainy_mail_domain and address != master_inbox:
+                self.output_connection.add_post_to_chain(int(inbox),
+                                                         mailfrom, data)
+                processed = True
+
+        if not processed:
+            chain_id = self.output_connection.create_chain(data, mailfrom)
+            self.output_connection.add_post_to_chain(chain_id, mailfrom, data)
 
 class OutputConnection(object):
     def create_chain(self, subject, users):
--- chainy_mail/chainymail_test.py
+++ chainy_mail/chainymail_test.py
@@ -1,7 +1,8 @@
 import chainymail
 import unittest
+import threading, asyncore, smtplib
 
-class TestChain(object):
+class TestableChain(object):
     def __init__(self, chain_id, subject, users):
         self.chain_id = chain_id
         self.subject = subject
@@ -10,29 +11,53 @@ class TestChain(object):
         self.posts = []
 
     def add_post(self, poster, body):
-        self.posts += (poster, body)
+        self.posts.append((poster, body))
 
-class TestOutputConnection(object):
+class TestableOutputConnection(object):
     def __init__(self):
-        self.chains = {}        
+        self.chains = {}
 
     def create_chain(self, subject, users):
         new_chain_id = len(self.chains)
-        self.chains[new_chain_id] = TestChain(new_chain_id, subject, users)
+        self.chains[new_chain_id] = TestableChain(new_chain_id, subject, users)
+        return new_chain_id
 
     def add_post_to_chain(self, chain_id, poster, body):
-        c = self.chains[chain_id]
+        c = self.chains[int(chain_id)]
         c.add_post(poster, body)
 
-class TestMailServer(unittest.TestCase):
-    def setUp(self):
-        self.output_connection = TestOutputConnection()
-        self.mail_server = chainymail.MailServer(self.output_connection)
+class MailServerThread(threading.Thread):
+    def __init__(self, output_connection):
+        threading.Thread.__init__(self)
 
-    def testNewChain(self):
-        self.mail_server.process_message('peer', 'mailfrom', 'rcpttos', 'data')
+        self.stop = False
+        self.server = chainymail.MailServer(output_connection)
+
+    def run(self):
+        while not self.stop:
+            asyncore.loop(timeout=1)
+
+class TestMailServerSystem(unittest.TestCase):
+    def setUp(self):
+        self.output_connection = TestableOutputConnection()
+
+        self.server_thread = MailServerThread(self.output_connection)
+        self.server_thread.start()
         
+        self.client = smtplib.SMTP()
+        #self.client.set_debuglevel(1)
+        self.client.connect(port=8001)
+
+    def testBasicMail(self):
+        msg = 'Hello world!\n'
+        self.client.sendmail('test1@test.com', 'master@chainy.neckbeard.ca', msg)
+        self.client.sendmail('test2@test.com', '0@chainy.neckbeard.ca', msg)
         self.assertEqual(len(self.output_connection.chains), 1)
+        self.assertEqual(len(self.output_connection.chains[0].posts), 2)
+        
+    def tearDown(self):
+        self.client.quit()
+        self.server_thread.stop = True
 
 if __name__ == '__main__':
     unittest.main()
